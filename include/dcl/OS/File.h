a11ce43fe41eb9405421acdb590cada53f5c68b4
WeZZard <me@wezzard.com>
2022-01-24 00:27:25 +0800

    Implements File and adds a test case.

--- include/dcl/OS/File.h
+++ include/dcl/OS/File.h
@@ -15,10 +15,92 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <cstdlib>
+#include <cstdint>
+#include <cassert>
+#include <memory>
+
 namespace dcl {
 
-namespace os {
+namespace OS {
 
-} // namespace dcl::os
+/**
+ * @brief `File` is a move-only resource.
+ * 
+ */
+class File {
+
+private:
+
+  int _fd;
+
+  void * _buffer;
+
+  size_t _size;
+
+#if DEBUG
+  char _path[PATH_MAX + 1];
+#endif
+
+public:
+
+  explicit File(const char * path, uint32_t flags);
+
+  ~File();
+
+  File(const File&) = delete;
+
+  File& operator = (const File&) = delete;
+
+  File(File&& another) {
+    * this = std::move(another);
+  }
+
+  File& operator = (File&& another) {
+    _buffer = another._buffer;
+    another._buffer = nullptr;
+    _size = another._size;
+    another._size = 0;
+    _fd = another._fd;
+    another._fd = -1;
+#if DEBUG
+    std::memset((void *)_path, 0, PATH_MAX + 1);
+    std::memcpy((void *)_path, another._path, strlen(another._path));
+    std::memset((void *)another._path, 0, PATH_MAX + 1);
+#endif
+    return *this;
+  }
+
+#pragma mark - Accessing File Contents
+
+public:
+
+  void * getBytes() {
+    return _buffer;
+  }
+
+  const void * getBytes() const {
+    return _buffer;
+  }
+
+  size_t getSize() {
+    return _size;
+  }
+
+  size_t getSize() const {
+    return _size;
+  }
+
+  int getFd() {
+    return _fd;
+  }
+
+  int getFd() const {
+    return _fd;
+  }
+
+};
+
+} // namespace dcl::OS
 
 } // namespace dcl
--- tests/unittests/dcl/OS/FileTests.cpp
+++ tests/unittests/dcl/OS/FileTests.cpp
@@ -1,1 +1,10 @@
 #include <gtest/gtest.h>
+
+#include <dcl/OS/File.h>
+
+TEST(File, ConstructsWithEmptyString) {
+  dcl::OS::File file { "", 0 };
+  EXPECT_EQ(file.getBytes(), nullptr);
+  EXPECT_EQ(file.getFd(), -1);
+  EXPECT_EQ(file.getSize(), 0);
+}
